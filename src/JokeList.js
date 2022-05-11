81d7d708a062a51bc7b02c1ff9195945bf9a5ec5
BrennenSchwab <brennen.schwab@gmail.com>
2022-05-10 17:31:52 -0700

    rev1-update JokeList.js

--- src/JokeList.js
+++ src/JokeList.js
@@ -13,67 +13,94 @@ class JokeList extends Component {
     this.state = {
       jokes: []
     };
-
-  useEffect(function() {
-    async function getJokes() {
-      let j = [...jokes];
-      let seenJokes = new Set();
-      try {
-        while (j.length < numJokesToGet) {
-          let res = await axios.get("https://icanhazdadjoke.com", {
-            headers: { Accept: "application/json" }
-          });
-          let { status, ...jokeObj } = res.data;
+    
+    this.generateNewJokes = this.generateNewJokes.bind(this);
+    this.vote = this.vote.bind(this);
+    
+  }
   
-          if (!seenJokes.has(jokeObj.id)) {
-            seenJokes.add(jokeObj.id);
-            j.push({ ...jokeObj, votes: 0 });
-          } else {
-            console.error("duplicate found!");
-          }
+  componentDidMount() {
+    if (this.state.jokes.length < this.props.numJokesToGet) this.getJokes();
+  }
+
+  componentDidUpdate() {
+    if (this.state.jokes.length < this.props.numJokesToGet) this.getJokes();
+  }
+
+  async getJokes() {
+    try {
+      let jokes = this.state.jokes;
+      let jokeVotes = JSON.parse(
+        window.localStorage.getItem("jokeVotes") || "{}"
+      );
+
+      let seenJokes = new Set(jokes.mp(j => j.id));
+
+      while (jokes.length < this.props.numJokesToGet) {
+        let res = await axios.get("https://icanhazdadjoke.com", {
+          headers: { Accept: "application/json" }
+        });
+        let { status, ...jokeObj } = res.data;
+
+        if (!seenJokes.has(jokeObj.id)) {
+          seenJokes.add(jokeObj.id);
+          jokeVotes[jokeObj.id] = jokeVotes[jokeObj.id] || 0;
+          jokes.push({ ...jokeObj, votes: jokeVotes[jokeObj.id] });
+        } else {
+          console.error("duplicate found!");
         }
-        setJokes(j);
-      } catch (e) {
-        console.log(e);
       }
+      this.setState({ jokes });
+      window.localStorage.setItem("jokeVotes", JSON.stringify(jokeVotes));
+    } catch (e) {
+      console.log(e);
     }
-
-    if (jokes.length === 0) getJokes();
-  }, [jokes, numJokesToGet]);
-
+  }
   /* empty joke list and then call getJokes */
 
-  function generateNewJokes() {
-    setJokes([]);
+  generateNewJokes() {
+    this.setState({ jokes: [] });
   }
 
   /* change vote for this id by delta (+1 or -1) */
 
-  function vote(id, delta) {
-    setJokes(allJokes =>
-      allJokes.map(j => (j.id === id ? { ...j, votes: j.votes + delta } : j))
-    );
-  }
+ vote(id, delta) {
+  let jokeVotes = JSON.parse(window.localStorage.getItem("jokeVotes"));
+  jokeVotes[id] = (jokeVotes[id] || 0) + delta;
+  window.localStorage.setItem("jokeVotes", JSON.stringify(jokeVotes));
+  this.setState(s => ({ jokes: s.jokes.map(j => j.id === id ? {...j, votes: j.votes + delta } : j )}))
+}
 
-  /* render: either loading spinner or list of sorted jokes. */
+render() {
+  let sortedJokes = [...this.state.jokes].sort((a, b) => b.votes - a.votes);
 
-  if (jokes.length) {
-    let sortedJokes = [...jokes].sort((a, b) => b.votes - a.votes);
-  
-    return (
-      <div className="JokeList">
-        <button className="JokeList-getmore" onClick={generateNewJokes}>
-          Get New Jokes
-        </button>
-  
-        {sortedJokes.map(j => (
-          <Joke text={j.joke} key={j.id} id={j.id} votes={j.votes} vote={vote} />
-        ))}
-      </div>
-    );
-  }
+  return (
+    <div className="JokeList">
+      <button
+        className="JokeList-getmore"
+        onClick={this.generateNewJokes}
+      >
+        Get New Jokes
+      </button>
 
-  return null;
+      {sortedJokes.map(j => (
+        <Joke
+          text={j.joke}
+          key={j.id}
+          id={j.id}
+          votes={j.votes}
+          vote={this.vote}
+        />
+      ))}
+
+      {sortedJokes.length < this.props.numJokesToGet ? (
+        <div className="loading">
+          <i className="fas fa-4x fa-spinner fa-spin" />
+        </div>
+      ) : null}
+    </div>
+  );
+}
 
 }
 
